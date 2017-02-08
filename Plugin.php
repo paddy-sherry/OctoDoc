00b643b88802c839b152ae6cd61880b47cae486a
paddysherry <hello@paddysherry.com>
2017-02-08 12:37:29 +0100

    added details to plugin.php

--- Plugin.php
+++ Plugin.php
@@ -4,6 +4,17 @@ use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'leaderinternet.documentation::lang.plugin.name',
+            'description' => 'leaderinternet.documentation::lang.plugin.description',
+            'author'      => 'Leader Internet',
+            'icon'        => 'oc-icon-book',
+            'homepage'    => 'https://leaderinternet.com'
+        ];
+    }
+
     public function registerComponents()
     {
     }
