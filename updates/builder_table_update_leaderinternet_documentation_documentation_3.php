a00c666f66ffbceb000f208fc79a01c4bdc5dc2a
paddysherry <hello@paddysherry.com>
2017-04-04 21:56:32 +0200

    increase content size to 30k chars

---
+++ updates/builder_table_update_leaderinternet_documentation_documentation_3.php
@@ -0,0 +1,27 @@
+<?php namespace LeaderInternet\Documentation\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateLeaderinternetDocumentationDocumentation2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('leaderinternet_documentation_documentation', function($table)
+        {
+            $table->string('content', 30000)->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('leaderinternet_documentation_documentation', function($table)
+        {
+            $table->string('content', 3000)->change();
+        });
+    }
+}
+
+
+
+
